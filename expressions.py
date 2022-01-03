fee61a5b05ca98e8fcec9f959dc1a6bbc787e343
Matt McGee <mlmcgeenc@gmail.com>
2022-01-03 12:06:44 -0500

    complete exercise 2.3

--- expressions.py
+++ expressions.py
@@ -1,2 +1,11 @@
+print('py4e exercises 2.2 and 2.3')
+
+# Exercise 2.2
 nzt = input('Enter your name: ')
-print('Hello, ',nzt)
+print('Hello, ', nzt)
+
+# Exercise 2.3
+hours = input('Enter your worked hours: ')
+rate = input('Enter your hourly rate: ')
+pay = float(rate) * float(hours)
+print('Pay', pay)
