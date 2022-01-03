1bd14ef71c6f9df7c68e3466de11246db826bdc5
Matt McGee <mlmcgeenc@gmail.com>
2022-01-03 13:51:47 -0500

    complete exercise 3.3

--- expressions.py
+++ expressions.py
@@ -2,3 +2,14 @@ print('py4e exercise 3.3')
 
 # Exercise 3.3
 score = input("Enter Score: ")
+floatScore = float(score)
+if floatScore >= 0.9:
+  print('A')
+elif floatScore >= 0.8:
+  print('B')
+elif floatScore >= 0.7:
+  print('C')
+elif floatScore >= 0.6:
+  print('D')
+else:
+  print('F')
