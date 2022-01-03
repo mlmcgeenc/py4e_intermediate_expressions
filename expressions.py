618988633a9e11557835f65d5a1f7bb14292fd95
Matt McGee <mlmcgeenc@gmail.com>
2022-01-03 13:40:18 -0500

    begin 3.3

--- expressions.py
+++ expressions.py
@@ -1,21 +1,4 @@
-print('py4e exercise 3.2')
+print('py4e exercise 3.3')
 
 # Exercise 3.3
-stringHours = input('Enter your worked hours:')
-stringRate = input('Enter your hourly rate:')
-try:
-  floatHours = float(stringHours)
-  floatRate = float(stringRate)
-except:
-  print('Error, please enter numeric input')
-  quit()
-
-if floatHours > 40 :
-  otHours = floatHours - 40
-  regPay = floatRate * 40
-  otPay = otHours * (floatRate * 1.5)
-  pay = regPay + otPay
-else:
-  pay = floatRate * floatHours
-
-print(pay)
+score = input("Enter Score: ")
