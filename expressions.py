dcc5e8c0a0c76e23407fbf5af48ae12617e6687e
Matt McGee <mlmcgeenc@gmail.com>
2022-01-04 10:19:28 -0500

    refactor pay calculation into a function

--- expressions.py
+++ expressions.py
@@ -1,21 +1,20 @@
 print('py4e exercise 4.6')
 
 # Exercise 4.6
+def computePay(hours, rate):
+  if hours > 40 :
+    otHours = hours - 40
+    regPay = rate * 40
+    otPay = otHours * (rate * 1.5)
+    pay = regPay + otPay
+  else:
+    pay = rate * hours
+  return pay
+    
 stringHours = input('Enter your worked hours:')
 stringRate = input('Enter your hourly rate:')
-try:
-  floatHours = float(stringHours)
-  floatRate = float(stringRate)
-except:
-  print('Error, please enter numeric input')
-  quit()
-
-if floatHours > 40 :
-  otHours = floatHours - 40
-  regPay = floatRate * 40
-  otPay = otHours * (floatRate * 1.5)
-  pay = regPay + otPay
-else:
-  pay = floatRate * floatHours
+floatHours = float(stringHours)
+floatRate = float(stringRate)
+pay = computePay(floatHours, floatRate)
 
 print(pay)
